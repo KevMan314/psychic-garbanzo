34ce168953d128b40c5a95159f4bf5dc558d4693
KevMan314 <mcgerveykevin@gmail.com>
2026-02-26 01:56:58 -0500

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var x = 1;          // code smell - use const/let
+var password = "abc123";   // SonarCloud will flag hardcoded credential
+
+function add(a, b) {
+    var result = a + b;
+    var result = a + b;   // duplicate variable - flagged as a bug
+    return result;
+}
+
+console.log(add(1, 2));
